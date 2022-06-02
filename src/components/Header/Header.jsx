7a39652a574ee1c66437e1c3c22a52681a16eb41
matheustanaka <matheustanakanog@gmail.com>
2022-06-02 18:29:58 -0300

    feat: add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "../Header/style.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>Task Manager</h1>
+        <button>Repository</button>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
