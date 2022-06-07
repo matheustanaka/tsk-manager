e6970583ddc8fcc3e7c58712257f9c49ef6c1aee
matheustanaka <matheustanakanog@gmail.com>
2022-06-07 18:54:43 -0300

    feat: add function to delete a task

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,6 +1,17 @@
 import "../TaskItem/TaskItem.scss";
+import { AiFillDelete } from "react-icons/ai";
+import axios from "axios";
+import { useAlert } from "react-alert";
 
 export const TaskItem = ({ task }) => {
+  const alert = useAlert();
+  const handleDeleteTask = async () => {
+    try {
+      await axios.delete(`http://localhost:8000/tasks/${task._id}`);
+    } catch (error) {
+      alert.error("Não foi possível deletar a tarefa");
+    }
+  };
   return (
     <div className="task-item-container">
       <div className="task-description">
@@ -17,6 +28,10 @@ export const TaskItem = ({ task }) => {
             className={task.isCompleted ? "checkmark completed" : "checkmark"}
           />
         </label>
+      </div>
+
+      <div className="delete">
+        <AiFillDelete size={18} color="#F97474" onClick={handleDeleteTask} />
       </div>
     </div>
   );
