8632a49f43bde5f62aa285c71031a50df8f8b0a8
matheustanaka <matheustanakanog@gmail.com>
2022-06-07 18:30:53 -0300

    feat: add value prop

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,12 +1,13 @@
 import "./CustomInput.scss";
 
-export const CustomInput = ({ onChange }) => {
+export const CustomInput = ({ onChange, value }) => {
   return (
     <div className="custom-input-container">
       <input
         type="text"
         className="custom-input"
         placeholder="Adicionar task ..."
+        value={value}
         onChange={(e) => onChange(e)}
       />
     </div>
