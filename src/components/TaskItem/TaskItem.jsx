07aa79918ec6e5e3230afa037732edaa5b26be5e
matheustanaka <matheustanakanog@gmail.com>
2022-06-06 22:22:28 -0300

    feat: add Task Item to check

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,8 +1,23 @@
+import "../TaskItem/TaskItem.scss";
+
 export const TaskItem = ({ task }) => {
   return (
-    <>
-      <h1>{task.description}</h1>
-      <h2>{task.isCompleted ? "Completa" : "Fudeu pae"}</h2>
-    </>
+    <div className="task-item-container">
+      <div className="task-description">
+        <label
+          className={
+            task.isCompleted
+              ? "checkbox-container-completed"
+              : "checkbox-container"
+          }
+        >
+          <h2>{task.description}</h2>
+          <input type="checkbox" defaultChecked={task.isCompleted} />
+          <span
+            className={task.isCompleted ? "checkmark completed" : "checkmark"}
+          />
+        </label>
+      </div>
+    </div>
   );
 };
