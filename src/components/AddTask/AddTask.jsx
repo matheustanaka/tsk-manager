bacd631b98a55caeffc98dcfbbf72ba2073590ee
matheustanaka <matheustanakanog@gmail.com>
2022-06-07 18:04:19 -0300

    feat: add alert provider

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
+// import axios from "axios";
+import { useAlert } from "react-alert";
 
 import { CustomInput } from "../CustomInput/CustomInput.jsx";
 import { CustomButton } from "../CustomButton/CustomButton.jsx";
@@ -9,14 +11,25 @@ import "./AddTask.scss";
 export const AddTask = () => {
   const [task, setTask] = useState("");
 
+  const alert = useAlert();
+
   const onChange = (e) => {
     setTask(e.target.value);
+  };
+
+  const handleTaskAddition = async () => {
+    try {
+      if (task.length === 0)
+        return alert.error("Tarefa precisa de uma descrição para ser criada");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="add-task-container">
       <CustomInput value={task} onChange={onChange} />
-      <CustomButton>
+      <CustomButton onClick={handleTaskAddition}>
         <FaPlus size={15} />
       </CustomButton>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 2500,
+  offset: "30px",
+  transition: transitions.SCALE,
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </React.StrictMode>
 );
